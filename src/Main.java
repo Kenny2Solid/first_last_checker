fa1e40dfc4917504ab6d1536f528fd7f0df64edf
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-09-26 13:37:50 -0400

    boo

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int lastDigit = number % 10;
+
+        int firstDigit = number;
+
+        while(number > 0){
+            firstDigit /= 10;
+        } return firstDigit + lastDigit;
+    }
 }
